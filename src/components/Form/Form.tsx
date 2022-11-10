106d732f2a86c3c3e0a21b27eaf7dfc2e0634a4b
Mateusz <michalikkontakt@gmail.com>
2022-11-10 14:43:17 +0100

    added new logic to form with react hook form lib

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,13 +1,28 @@
 import React, { useState, useContext, useId } from 'react'
 import { useNavigate } from 'react-router-dom'
 import classes from './Form.module.scss'
-import { MainButton } from '../UI/MainButton'
 import { Note } from '../../types/NoteType'
 import { EditHistory } from '../../types/EditHistoryType'
 import { NotesContext } from '../../context/NoteContext'
 import { getFullDate } from '../../constants/FullDate'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
+import { useForm, SubmitHandler } from 'react-hook-form'
+import { SelectChangeEvent } from '@mui/material'
+
+type Inputs = {
+	title: string
+	description: string
+	category: string
+}
 
 export const Form = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<Inputs>()
+
 	const [isFavourite, setIsFavourite] = useState(true)
 	const [editHistory] = useState<EditHistory[]>([])
 	const { addNote } = useContext(NotesContext)
@@ -15,29 +30,66 @@ export const Form = () => {
 	const id = useId()
 	const { fullDate } = getFullDate()
 
-	const submitFormHandler = (e: React.FormEvent) => {
-		e.preventDefault()
+	const onSubmitForm: SubmitHandler<Inputs> = (data) => {
+		const NoteObj: Note = {
+			id: id,
+			author: 'Mateusz',
+			title: data.title,
+			category: data.category,
+			description: data.description,
+			favourite: isFavourite,
+			date: fullDate,
+			descLength: data.description.length,
+			editHistory: editHistory,
+		}
 
-		// if (!allInputsIsValid) {
-		// 	return
-		// }
+		console.log(NoteObj)
 
-		// const NoteObj: Note = {
-		// 	id: id,
-		// 	author: enteredAuthor,
-		// 	title: enteredTitle,
-		// 	category: enteredCategory,
-		// 	description: enteredDescription,
-		// 	favourite: isFavourite,
-		// 	date: fullDate,
-		// 	descLength: enteredDescription.length,
-		// 	editHistory: editHistory,
-		// }
-
-		// addNote(NoteObj)
+		addNote(NoteObj)
 
 		navigate('/notes')
 	}
 
-	return <div></div>
+	return (
+		<form className={classes.form} onSubmit={handleSubmit(onSubmitForm)}>
+			<div className={classes.formControl}>
+				<label htmlFor='title'>Title</label>
+				<input id='title' type='text' {...register('title', { required: true, min: 3, max: 20 })} />
+			</div>
+			{errors.title && <span className={classes.errorMessage}>This field is required</span>}
+
+			<div className={classes.formControl}>
+				<label htmlFor='description'>Description</label>
+				<textarea id='description' {...register('description', { required: true, min: 10, max: 50 })} />
+			</div>
+
+			<div className={classes.formControl}>
+				<label htmlFor='category'>Category</label>
+				<select defaultValue='Choose category' {...register('category')} id='category'>
+					<option disabled value='Choose category'>
+						Choose category
+					</option>
+					<option value={'Shopping'}>Shopping</option>
+					<option value={'Traveling'}>Traveling</option>
+					<option value={'Business'}>Business</option>
+					<option value={'Cooking'}>Cooking</option>
+				</select>
+			</div>
+			{errors.category && <span className={classes.errorMessage}>This field is required</span>}
+
+			<div className={classes.formControl}>
+				<FormControlLabel
+					control={
+						<Checkbox
+							defaultChecked
+							onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIsFavourite(e.target.checked)}
+						/>
+					}
+					label={isFavourite ? 'Is favourite' : 'Not favourite'}
+				/>
+			</div>
+
+			<button type='submit'>Submit</button>
+		</form>
+	)
 }
