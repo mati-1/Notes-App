4d3a1d3ca13a3238d270c718cbe16721bae44128
Mateusz <michalikkontakt@gmail.com>
2022-11-13 02:27:06 +0100

    added dummy user profile to note

--- src/components/Notes/SingleNoteDetails.tsx
+++ src/components/Notes/SingleNoteDetails.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import classes from './NoteItem.module.scss'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import Avatar from '@mui/material/Avatar'
 import { EditHistory } from './../../types/EditHistoryType'
+import { AuthContext } from '../../context/AuthContext'
 
 type noteDetailsType = {
 	id: string | undefined
@@ -13,6 +14,8 @@ type noteDetailsType = {
 }
 
 export const SingleNoteDetails = ({ date, id, favourite, editHistory }: noteDetailsType) => {
+	const { isLoggedIn } = useContext(AuthContext)
+
 	return (
 		<div className={classes.details}>
 			<h2 className={classes.heading}>Details</h2>
@@ -40,13 +43,23 @@ export const SingleNoteDetails = ({ date, id, favourite, editHistory }: noteDeta
 
 				<div className={classes.detailsControls}>
 					<h3>Author</h3>
-					<Link to='/user' className={classes.user}>
-						<Avatar
-							alt='user profile'
-							src='https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/277465687_1630667890617860_6404384895161569634_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HQ6CITGFdowAX_iS7LP&_nc_ht=scontent-frx5-1.xx&oh=00_AfCypr7OTxIUIcJ_xmN5P3kjUeeEQSsiPQ72HwA8EWVE-Q&oe=636D10CC'
-						/>
-						<p>Mateusz</p>
-					</Link>
+					{isLoggedIn ? (
+						<Link to='/user' className={classes.user}>
+							<Avatar
+								alt='user profile'
+								src='https://scontent-frx5-1.xx.fbcdn.net/v/t39.30808-6/277465687_1630667890617860_6404384895161569634_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=HQ6CITGFdowAX_iS7LP&_nc_ht=scontent-frx5-1.xx&oh=00_AfCypr7OTxIUIcJ_xmN5P3kjUeeEQSsiPQ72HwA8EWVE-Q&oe=636D10CC'
+							/>
+							<p>Mateusz</p>
+						</Link>
+					) : (
+						<Link className={classes.user} to='/login'>
+							<Avatar
+								alt='user profile'
+								src='https://www.szinhaz.szeged.hu/sites/default/files/default_images/no_avatar_57.jpg'
+							/>
+							<p>Guest</p>
+						</Link>
+					)}
 				</div>
 			</div>
 		</div>
