ce3b70e68cbbf256fecd4d396199a9d38fcadaf6
Mateusz <michalikkontakt@gmail.com>
2022-11-14 00:30:24 +0100

    add: added logic to fetch user data, not completly

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,27 +1,59 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { UserData } from './../types/UserDataType'
+import { collection, query, where, getDocs, addDoc } from 'firebase/firestore'
+import { db } from '../firebase'
 
 type AuthContextType = {
 	token: string
+	userData: UserData
 	isLoggedIn: boolean
-	login: (userData: UserData) => void
+	loginUser: (token: string) => void
+	registerUser: (userData: UserData, token: string) => void
 	logout: () => void
 }
 
 export const AuthContext = React.createContext<AuthContextType>({
 	token: '',
+	userData: {},
 	isLoggedIn: false,
-	login: () => {},
+	loginUser: () => {},
+	registerUser: () => {},
 	logout: () => {},
 })
 
-export const AuthContextProvider = ({ children }: { children: JSX.Element }) => {
+export const AuthContextProvider = ({ children }: { children: React.ReactNode }) => {
+	const [initialData, setInitialData] = useState<UserData>({})
 	const [token, setToken] = useState('')
 	const userIsLoggedIn = !!token
 
-	const loginHandler = (userData: UserData) => {
-		setToken(userData.token)
-	}
+	const loginHandler = useCallback(async (token: string) => {
+		const q = query(collection(db, 'users'), where('token', '==', token))
+
+		const querySnapshot = await getDocs(q)
+		querySnapshot.forEach((doc) => {
+			// doc.data() is never undefined for query doc snapshots
+			console.log(doc.id, '  =>  ', doc.data())
+
+			setInitialData({
+				...doc.data(),
+				id: doc.id,
+			})
+		})
+
+		setToken(token)
+	}, [])
+
+	const registerHandler = useCallback(async (userData: UserData, token: string) => {
+		const user = {
+			...userData,
+			token: token,
+		}
+
+		console.log('register')
+		console.log(user)
+
+		await addDoc(collection(db, 'users'), user)
+	}, [])
 
 	const logoutHandler = () => {
 		setToken('')
@@ -29,8 +61,10 @@ export const AuthContextProvider = ({ children }: { children: JSX.Element }) => 
 
 	const contextValue = {
 		token: token,
+		userData: initialData,
 		isLoggedIn: userIsLoggedIn,
-		login: loginHandler,
+		loginUser: loginHandler,
+		registerUser: registerHandler,
 		logout: logoutHandler,
 	}
 	return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>
