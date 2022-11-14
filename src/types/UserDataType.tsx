460c1f4b3bd01e9bb340adabec28dc0c91216727
Mateusz <michalikkontakt@gmail.com>
2022-11-14 19:34:33 +0100

    fix: fixed types of optional modules inside interfae

--- src/types/UserDataType.tsx
+++ src/types/UserDataType.tsx
@@ -1,9 +1,9 @@
 export interface UserData {
-	readonly id: string
+	readonly id?: string
 	readonly name: string
 	readonly surname: string
 	readonly email: string
 	readonly password: string
-	readonly token: string
+	readonly token?: string
 	readonly returnSecureToken: boolean
 }
