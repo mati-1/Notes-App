bd4caf3ee2d13166a34ab3ae364c603346da1542
Mateusz <michalikkontakt@gmail.com>
2022-11-14 22:43:28 +0100

    update: added new type

--- src/types/UserDataType.tsx
+++ src/types/UserDataType.tsx
@@ -1,9 +1,11 @@
 export interface UserData {
-	readonly id?: string
+	readonly id: string
 	readonly name: string
 	readonly surname: string
 	readonly email: string
 	readonly password: string
-	readonly token?: string
+	readonly token: string
 	readonly returnSecureToken: boolean
+	readonly created: string
+	readonly lastLogin: string
 }
