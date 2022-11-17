a8c02a10854aca3a8c732e4ab28730d835c4cc58
Mateusz <michalikkontakt@gmail.com>
2022-11-18 00:58:12 +0100

    fix: added hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<StrictMode>
-		<App />
-	</StrictMode>
+	<HashRouter>
+		<StrictMode>
+			<App />
+		</StrictMode>
+	</HashRouter>
 )
 
 reportWebVitals()
