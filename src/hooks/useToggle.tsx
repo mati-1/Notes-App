ed81f80490548f1b25f945c587838ba76cd1091d
Mateusz <michalikkontakt@gmail.com>
2022-11-17 22:29:46 +0100

    feat: finished app

--- src/hooks/useToggle.tsx
+++ src/hooks/useToggle.tsx
@@ -1,10 +1,8 @@
 import { useCallback, useState } from 'react'
 
 export const useToggle = (initialState: boolean = false): [boolean, any] => {
-	// Initialize the state
 	const [state, setState] = useState<boolean>(initialState)
-	// Define and memorize toggler function in case we pass down the comopnent,
-	// This function change the boolean value to it's opposite value
+
 	const toggle = useCallback((): void => setState((state) => !state), [])
 	return [state, toggle]
 }
