e4b2909a4ebeb36f9d1d5b3650c8b9a5c41c3f6e
Mateusz <michalikkontakt@gmail.com>
2022-11-18 01:49:44 +0100

    fix: fixed hash router

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.scss'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<StrictMode>
-		<HashRouter>
-			<App />
-		</HashRouter>
+		<App />
 	</StrictMode>
 )
 
